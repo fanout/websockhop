a5ef15bd439e8ea2970c34fcddd7781a7bc767cc
Katsuyuki Ohmuro <harmony7@pex2.jp>
2016-10-19 23:15:02 +0900

    Unify logging
    WorkItem: #18

--- src/WebSockHop.js
+++ src/WebSockHop.js
@@ -68,7 +68,7 @@ class WebSockHop {
             if (this._tries > 0) {
                 const timeCap = 1 << Math.min(6, (this._tries - 1));
                 delay = timeCap * 1000 + Math.floor(Math.random() * 1000);
-                console.info(`WebSockHop: Trying again in ${delay}ms`);
+                WebSockHop.log("info", `Trying again in ${delay}ms`);
             }
             this._tries = this._tries + 1;
 
@@ -86,13 +86,13 @@ class WebSockHop {
         this._aborted = true;
     }
     async _raiseEvent(event, ...args) {
-        console.log(`WebSockHop: ${event} event start`);
+        WebSockHop.log("log", `${event} event start`);
         await this._events.trigger(event, ...args);
-        console.log(`WebSockHop: ${event} event end`);
+        WebSockHop.log("log", `${event} event end`);
     }
     async _start() {
         if (this.formatter == null) {
-            console.log("No message formatter had been specified, creating a StringFormatter instance as formatter.");
+            WebSockHop.log("log", "No message formatter had been specified, creating a StringFormatter instance as formatter.");
             this.formatter = new StringFormatter();
         }
         await this._raiseEvent("opening");
@@ -102,20 +102,20 @@ class WebSockHop {
             let connectionTimeout = null;
             if (this.connectionTimeoutMsecs) {
                 connectionTimeout = setTimeout(() => {
-                    console.log("WebSockHop: Connection timeout exceeded.");
+                    WebSockHop.log("log", "Connection timeout exceeded.");
                     this._raiseErrorEvent(false);
                 }, this.connectionTimeoutMsecs);
-                console.log(`WebSockHop: Setting connection timeout (${this.connectionTimeoutMsecs} msecs).`);
+                WebSockHop.log("log", `Setting connection timeout (${this.connectionTimeoutMsecs} msecs).`);
             }
             const clearConnectionTimeout = () => {
                 if (connectionTimeout != null) {
-                    console.log("WebSockHop: Clearing connection timeout.");
+                    WebSockHop.log("log", "Clearing connection timeout.");
                     clearTimeout(connectionTimeout);
                     connectionTimeout = null;
                 }
             };
             this.socket.onopen = async () => {
-                console.log("WebSockHop: WebSocket::onopen");
+                WebSockHop.log("log", "WebSocket::onopen");
                 clearConnectionTimeout();
                 this.protocol = this.socket.protocol;
                 this._tries = 0;
@@ -123,7 +123,7 @@ class WebSockHop {
                 this._resetPingTimer();
             };
             this.socket.onclose = ({wasClean, code}) => {
-                console.log(`WebSockHop: WebSocket::onclose { wasClean: ${wasClean ? "true" : "false"}, code: ${code} }`);
+                WebSockHop.log("log", `WebSocket::onclose { wasClean: ${wasClean ? "true" : "false"}, code: ${code} }`);
                 clearConnectionTimeout();
                 const closing = this._closing;
 
@@ -141,7 +141,7 @@ class WebSockHop {
             };
             this.socket.onmessage = ({data}) => {
                 nextUpdate(() => {
-                    console.log(`WebSockHop: WebSocket::onmessage { data: ${data} }`);
+                    WebSockHop.log("log", `WebSocket::onmessage { data: ${data} }`);
                     this._dispatchMessage(data);
                 });
             };
@@ -164,19 +164,19 @@ class WebSockHop {
         }
     }
     _clearPingTimers() {
-        console.log("WebSockHop: clearing ping timers.");
+        WebSockHop.log("log", "clearing ping timers.");
         if (this._pingTimer) {
             clearTimeout(this._pingTimer);
             this._pingTimer = null;
         }
     }
     _resetPingTimer() {
-        console.log("WebSockHop: resetting ping timer.");
+        WebSockHop.log("log", "resetting ping timer.");
         this._clearPingTimers();
         this._pingTimer = setTimeout(() => {
             this.sendPingRequest();
         }, this.pingIntervalMsecs);
-        console.log(`WebSockHop: attempting ping in ${this.pingIntervalMsecs} ms`);
+        WebSockHop.log("log", `attempting ping in ${this.pingIntervalMsecs} ms`);
     }
     sendPingRequest() {
         if (this.formatter != null) {
@@ -188,18 +188,18 @@ class WebSockHop {
                     this._lastReceivedPongId = obj.id;
                 }, error => {
                     if (error.type == ErrorEnumValue.Timeout) {
-                        console.log("WebSockHop: no ping response, handling as disconnected");
+                        WebSockHop.log("log", "no ping response, handling as disconnected");
                     }
                 }, this.pingResponseTimeoutMsecs, true);
-                console.log(`WebSockHop: > PING [${ping.id}], requiring response in ${this.pingResponseTimeoutMsecs} ms`);
+                WebSockHop.log("log", `> PING [${ping.id}], requiring response in ${this.pingResponseTimeoutMsecs} ms`);
             } else if (pingMessage != null) {
                 this._lastSentPing = this._sendPingMessage(pingMessage, this.pingResponseTimeoutMsecs);
-                console.log(`WebSockHop: > PING, requiring response in ${this.pingResponseTimeoutMsecs} ms`);
+                WebSockHop.log("log", `> PING, requiring response in ${this.pingResponseTimeoutMsecs} ms`);
             } else {
-                console.log("WebSockHop: No ping set up for message formatter, not performing ping.");
+                WebSockHop.log("log", "No ping set up for message formatter, not performing ping.");
             }
         } else {
-            console.log("WebSockHop: Time for ping, but no formatter selected, not performing ping.");
+            WebSockHop.log("log", "Time for ping, but no formatter selected, not performing ping.");
         }
     }
     send(obj) {
@@ -222,7 +222,7 @@ class WebSockHop {
     }
     abort() {
         if (this.socket) {
-            console.log("WebSockHop: abort() called on live socket, performing forceful shutdown.  Did you mean to call close() ?");
+            WebSockHop.log("log", "abort() called on live socket, performing forceful shutdown.  Did you mean to call close() ?");
             this._clearPingTimers();
             this._lastSentPing = null;
             this._lastReceivedPongId = 0;
@@ -282,7 +282,7 @@ class WebSockHop {
         const { obj: { id } } = request;
         request.clearTimeout();
         request.requestTimeoutTimer = setTimeout(async () => {
-            console.log(`WebSockHop: timeout exceeded [${id}]`);
+            WebSockHop.log("log", `timeout exceeded [${id}]`);
             await this._dispatchErrorMessage(id, {type: ErrorEnumValue.Timeout});
             if (request.requestDisconnectOnTimeout) {
                 await this._raiseErrorEvent(false);
@@ -312,7 +312,7 @@ class WebSockHop {
 
         pingMessage.clearTimeout();
         pingMessage.messageTimeoutTimer = setTimeout(() => {
-            console.log("WebSockHop: timeout exceeded");
+            WebSockHop.log("log", "timeout exceeded");
             this._raiseErrorEvent(false);
         }, pingMessage.messageTimeoutMsecs);
 
@@ -348,7 +348,7 @@ class WebSockHop {
 
                 // If this is NOT a pong then just extend the response timer, if any
                 if (!isPong && this._lastSentPing != null) {
-                    console.log("WebSockHop: Non-pong received during pong response period, extending delay...");
+                    WebSockHop.log("log", "Non-pong received during pong response period, extending delay...");
                     this._startRequestTimeout(this._lastSentPing);
                 }
 
@@ -374,7 +374,7 @@ class WebSockHop {
                         this._lastSentPing = null;
                     } else {
                         // If this is NOT a pong then just extend the response timer, if any
-                        console.log("WebSockHop: Non-pong received during pong response period, extending delay...");
+                        WebSockHop.log("log", "Non-pong received during pong response period, extending delay...");
                         this._startPingMessageTimeout(this._lastSentPing);
                     }
                 }
@@ -382,16 +382,16 @@ class WebSockHop {
 
             if (isPong) {
                 if (pongId > 0) {
-                    console.log("WebSockHop: < PONG [" + pongId + "]");
+                    WebSockHop.log("log", "< PONG [" + pongId + "]");
                 } else {
-                    console.log("WebSockHop: < PONG");
+                    WebSockHop.log("log", "< PONG");
                 }
                 this._resetPingTimer();
             }
             if (!isHandled && isFunction(this.formatter.handlePing)) {
                 const isPing = await Promise.resolve(this.formatter.handlePing(obj));
                 if (isPing) {
-                    console.log("WebSockHop: Received PING message, handled.");
+                    WebSockHop.log("log", "Received PING message, handled.");
                     isHandled = true;
                 }
             }
@@ -424,6 +424,19 @@ class WebSockHop {
         return true;
     }
 }
+WebSockHop.logger = null;
+if (process.env.NODE_ENV === 'development') {
+    WebSockHop.logger = (type, message) => {
+        console.log(`WebSockHop: ${type} - ${message}`);
+    };
+}
+
+WebSockHop.log = (type, message) => {
+    if (WebSockHop.logger != null) {
+        WebSockHop.logger(type, message);
+    }
+};
+
 WebSockHop.disable = { oldSafari: true, mobile: true };
 WebSockHop.ErrorEnumValue = ErrorEnumValue;
 WebSockHop.StringFormatter = StringFormatter;
